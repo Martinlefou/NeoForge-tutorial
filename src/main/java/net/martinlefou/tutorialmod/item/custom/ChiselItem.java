d9aac109dc885370d2f21ceea7394b198bec3c17
Martinlefou <guenardmartin966@gmail.com>
2024-09-15 10:43:49 +0200

    tasty tooltips ! yummmy

--- src/main/java/net/martinlefou/tutorialmod/item/custom/ChiselItem.java
+++ src/main/java/net/martinlefou/tutorialmod/item/custom/ChiselItem.java
@@ -1,17 +1,22 @@
 package net.martinlefou.tutorialmod.item.custom;
 
 import net.martinlefou.tutorialmod.block.ModBlocks;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
+import java.util.List;
 import java.util.Map;
 
 public class ChiselItem extends Item {
@@ -44,5 +49,15 @@ public class ChiselItem extends Item {
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        if (Screen.hasShiftDown()) {
+            tooltipComponents.add(Component.translatable("tooltip.tutorialmod.chisel.shift_down"));
+        } else {
+            tooltipComponents.add(Component.translatable("tooltip.tutorialmod.chisel"));
+        }
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
