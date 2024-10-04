1e30f37960b20600eeaf385bea1c832cd05ea6d0
Martinlefou <guenardmartin966@gmail.com>
2024-10-04 22:41:12 +0200

    mineable blocks

--- src/main/java/net/martinlefou/tutorialmod/datagen/ModBlockTagProvider.java
+++ src/main/java/net/martinlefou/tutorialmod/datagen/ModBlockTagProvider.java
@@ -20,15 +20,23 @@ public class ModBlockTagProvider extends BlockTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider provider) {
         tag(BlockTags.MINEABLE_WITH_PICKAXE)
-                .add(ModBlocks.BISMUTH_BLOCK.get());
-                add(ModBlocks.BISMUTH_ORE.get());
-                add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get());
-                add(ModBlocks.MAGIC_BLOCK.get());
+                .add(ModBlocks.BISMUTH_BLOCK.get())
+                .add(ModBlocks.BISMUTH_ORE.get())
+                .add(ModBlocks.BISMUTH_LAMP.get())
+                .add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get());
+
+        tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.BISMUTH_DEEPSLATE_ORE.get());
+
+        tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.BISMUTH_LAMP.get());
 
         tag(BlockTags.FENCES).add(ModBlocks.BISMUTH_FENCE.get());
         tag(BlockTags.FENCE_GATES).add(ModBlocks.BISMUTH_FENCE_GATE.get());
         tag(BlockTags.WALLS).add(ModBlocks.BISMUTH_WALL.get());
+
     }
+
 
 
     private void add(Block block) {
