b066d7408e918f1786ef15f059443ce6f5ec4da8
Martinlefou <guenardmartin966@gmail.com>
2024-10-09 15:37:08 +0200

    armor & armor effects

--- src/main/java/net/martinlefou/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/martinlefou/tutorialmod/item/ModCreativeModeTabs.java
@@ -40,6 +40,11 @@ public class ModCreativeModeTabs {
 
                             output.accept(ModItems.BISMUTH_HAMMER);
 
+                            output.accept(ModItems.BISMUTH_HELMET);
+                            output.accept(ModItems.BISMUTH_CHESTPLATE);
+                            output.accept(ModItems.BISMUTH_LEGGINGS);
+                            output.accept(ModItems.BISMUTH_BOOTS);
+
                         }).build());
     }
 
