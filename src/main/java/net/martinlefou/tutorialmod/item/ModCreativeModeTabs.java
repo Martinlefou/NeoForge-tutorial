5b8f1dd32c1eea0256bb02e2503cfc9a6bbe8db0
Martinlefou <guenardmartin966@gmail.com>
2024-10-12 09:55:04 +0200

    bow

--- src/main/java/net/martinlefou/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/martinlefou/tutorialmod/item/ModCreativeModeTabs.java
@@ -49,6 +49,8 @@ public class ModCreativeModeTabs {
 
                             output.accept(ModItems.KAUPEN_SMITHING_TEMPLATE);
 
+                            output.accept(ModItems.KAUPEN_BOW);
+
                         }).build());
     }
 
